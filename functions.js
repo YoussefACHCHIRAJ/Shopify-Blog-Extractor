ccca35a5b96a90b25556db3aa7888c86ee60caeb
Youssef ACHCHIRAJ <youssefachchiraj@gmail.com>
2024-10-21 14:19:27 +0100

    feat: add the export to word format

---
+++ functions.js
@@ -0,0 +1,88 @@
+const { Document, Packer, Paragraph, TextRun } = require("docx");
+const fs = require("fs");
+const { Parser } = require("json2csv");
+
+const toCsvs = (articles) => {
+    const folderName = `uploads/csvs`;
+
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName, { recursive: true });
+  }
+  const fields = [
+    "title",
+    "body_html",
+    "summary_html",
+    "published_at",
+    "image",
+    "author",
+    "handle",
+  ];
+  const parser = new Parser({ fields });
+  const csv = parser.parse(articles);
+  const formatedFileName = `${folderName}/shopify_blogs_${Date.now()}.csv`;
+  // Save the CSV to a file
+  fs.writeFile(formatedFileName, csv, (err) => {
+    if (err) throw err;
+    console.log(`Blog data has been exported to ${formatedFileName}`);
+  });
+};
+
+const toWord = (articles) => {
+  const folderName = `uploads/words/word_${Date.now()}`;
+
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName, { recursive: true });
+  }
+
+  articles.forEach((article, _index) => {
+    // Create a new Word document for each article
+    
+
+    // Create paragraphs for the title and body
+    const title = new Paragraph({
+      children: [
+        new TextRun({
+          text: article.title,
+          bold: true,
+          size: 28, // Title size
+        }),
+      ],
+      spacing: { after: 300 },
+    });
+
+    const body = new Paragraph({
+      children: [
+        new TextRun({
+          text: article.body_html, 
+        }),
+      ],
+      spacing: { after: 300 },
+    });
+
+    const doc = new Document({
+        creator: "Youssef ACHCHIRAJ - SHOPIFY EXPORT APP",
+        title: article.title,
+        description: `Exported blog article titled: ${article.title}`,
+        sections: [{
+            children: [title, body]
+        }]
+      });
+    
+      Packer.toBuffer(doc)
+      .then((buffer) => {
+        const savedBlogName = `${folderName}/shopify_blogs_${Date.now()}.docx`;
+        fs.writeFileSync(savedBlogName, buffer);
+        console.log(
+          `Article "${article.title}" has been exported to ${savedBlogName}`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error generating Word document for article "${article.title}":`,
+          error
+        );
+      });
+  });
+};
+
+module.exports = { toCsvs, toWord };
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
-const fs = require("fs");
-const { Parser } = require("json2csv");
 const dotenv = require("dotenv");
+const { toCsvs, toWord } = require("./functions");
 
 dotenv.config();
 
@@ -26,34 +25,15 @@ const url = `https://${API_KEY}:${PASSWORD}@${SHOP_NAME}/admin/api/2024-01/artic
 axios
   .get(url)
   .then((response) => {
-    const articles = response.data.articles;
-    // Prepare the fields you want to export
-    const fields = [
-      "title",
-      "body_html",
-      "summary_html",
-      "published_at",
-      "image",
-      "author",
-      "handle",
-    ];
-    const parser = new Parser({ fields });
+    const articles = response.data.articles;    
     const formattedArticle = articles.map((article) => ({
-      title: article.title,
-      body_html: article.body_html,
-      published_at: article.published_at,
-      author: article.author,
-      handle: article.handle,
+      ...article,
       image: article.image ? article.image.src : "",
     }));
 
     try {
-      const csv = parser.parse(formattedArticle);
-      // Save the CSV to a file
-      fs.writeFile(`uploads/shopify_blogs_${Date.now()}.csv`, csv, (err) => {
-        if (err) throw err;
-        console.log("Blog data has been exported to shopify_blogs.csv");
-      });
+      // toCsvs(parser, formattedArticle);
+      toWord(formattedArticle);
     } catch (err) {
       console.error("Error generating CSV:", err);
     }
